d2184552f19d723dab0304354e95cf5161623958
Christopher Käck <christopher.kack@gmail.com>
2012-03-28 18:54:02 +0200

    Fixed typos

--- readmill.py
+++ readmill.py
@@ -1,25 +1,25 @@
 import requests, json
 class Readmill():
-    CLIENTID = "95cc31aac4a26da8b5c67388120428b4"
-    baseUrl = "http://api.readmill.com"
+    def __init__(self):
+        self.CLIENTID = "95cc31aac4a26da8b5c67388120428b4"
+        self.baseUrl = "http://api.readmill.com"
     
     def getUser(self, id):
-        r = requests.get( baseUrl + "/users/"+str(id)+"?client_id="+CLIENTID)
+        r = requests.get( self.baseUrl + "/users/"+str(id)+"?client_id="+self.CLIENTID)
         if r.status_code == 200: 
             return json.loads(r.content)
         else: 
             raise Exception, "Error Occured: " + str(r.error)
-    def
     
-    def getBook(id):
-        r = requests.get( baseUrl + "/books/"+str(id)+"?client_id="+CLIENTID)
+    def getBook(self, id):
+        r = requests.get( self.baseUrl + "/books/"+str(id)+"?client_id="+self.CLIENTID)
         if r.status_code == 200: 
             return json.loads(r.content)
         else: 
             raise Exception, "Error Occured: " + str(r.error)
             
-    def findBook(title=None, author=None, isbn=None)
-        baseStr = baseUrl + "/books/match?client_id="+CLIENTID
+    def findBook(self, title=None, author=None, isbn=None):
+        baseStr = self.baseUrl + "/books/match?client_id="+self.CLIENTID
         params = []
         if title is not None:
             params.append("&q[title]=" + title)
