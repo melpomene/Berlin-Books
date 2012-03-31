4a5c563c45979a3e8507154354ce0dd9f1725b4e
Christopher Käck <christopher.kack@gmail.com>
2012-03-31 12:48:12 +0200

    nice

--- view.py
+++ view.py
@@ -10,8 +10,8 @@ render = web.template.render('templates/')
 
 def index(**k):
 	fb = oauth2.FacebookAuth(FACEBOOK_ID, FACEBOOK_SECRET, "http://localhost:8080/callback")
-	return render.index("test", fb.auth_string)
+	return render.index(auth=fb.auth_string)
 
 def callback(**k):
 	oauth2.FacebookAuth(FACEBOOK_ID, FACEBOOK_SECRET, "http://localhost:8080/callback")
-	return render.callback(k['code'])
+	return render.callback(code = k['code'])
