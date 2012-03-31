48ed31db22274acd333c6b8783059df9b6c4851a
Christopher Käck <christopher.kack@gmail.com>
2012-03-31 20:52:22 +0200

    simple recommendation done

--- recommend.py
+++ recommend.py
@@ -36,10 +36,31 @@ class Recommend():
 			else:
 				user.append(0)
 		self.user = user
-		print user
+		
 
+	def compare(self):
+		greatest = 0
+		greatest_user = None
+		for user in self.matrix:
+			total = 0
+			for i in xrange(len(self.user)):
+				if self.user[i] == 1 and user[i] == 1: 
+					total += 1
+			if total > greatest:
+				greatest = total
+				greatest_user = user
+
+		print "You: \t\t\t" + str(self.user)
+		print "Your best match: \t" + str(greatest_user)
+		print "Maybe you have missed out these titles:"
+		for i in xrange(len(self.user)):
+			if self.user[i] == 0 and greatest_user[i] == 1:
+				print self.book_dictionary[i] 
+				
+		
 if __name__ == "__main__":
 	r = Recommend()
-	r.build_dict([{"name":"tom", "books":["bok1", "bok2"]},{"name":"eva", "books":["bok1"] } ])
+	r.build_dict([{"name":"tom", "books":["bok1", "bok2", "bok3"]}, {"name":"eva", "books":["bok1", "bok4"] } ])
 	r.build_user(["bok1", "bok2"])
+	r.compare()
 
